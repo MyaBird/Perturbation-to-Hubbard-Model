4b24bc980ff19fa6b15aabf359f95c510f8c34cf
Mya Shekitka <myabird1201@vt.edu>
2026-05-12 13:41:16 -0400

    Modified testing

--- tests/test_calculate_thetas.py
+++ tests/test_calculate_thetas.py
@@ -1,6 +1,14 @@
 import perturbation_to_hubbard_model
-import numpy as np
 import pytest
+import numpy as np
 
 def test_calculate_thetas():
     
+    thetas = perturbation_to_hubbard_model.calculate_thetas(1,0,1,2,3,4,5)
+
+    assert(np.isclose(thetas.theta_0, -1.047))
+    assert(np.isclose(thetas.theta_2, 0.542))
+    assert(np.isclose(thetas.theta_gs, 0.505))
+    assert(np.isclose(thetas.theta_neg1, -2.094))
+    assert(np.isclose(thetas.theta_h, 2.739))
+    assert(np.isclose(thetas.theta_1, 0.368))
--- tests/test_pt_to_hubbard.py
+++ tests/test_pt_to_hubbard.py
@@ -1,7 +1,7 @@
 """Tests for `perturbation_to_hubbard_model` package."""
 
 import perturbation_to_hubbard_model
-
+import pytest
 
 def test_import():
     """Verify the package can be imported."""
