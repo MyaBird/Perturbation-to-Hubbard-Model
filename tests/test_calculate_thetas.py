4acd50e37d98c1c4ca899dc1200873a30f3bd7d7
Mya Shekitka <myabird1201@vt.edu>
2026-05-12 17:13:28 -0400

    Modified testing

--- tests/test_calculate_thetas.py
+++ tests/test_calculate_thetas.py
@@ -6,9 +6,9 @@ def test_calculate_thetas():
     
     thetas = perturbation_to_hubbard_model.CalculateThetas(1,0,1,2,3,4,5)
 
-    assert(np.isclose(thetas.theta_0, -1.047))
-    assert(np.isclose(thetas.theta_2, 0.542))
-    assert(np.isclose(thetas.theta_gs, 0.505))
-    assert(np.isclose(thetas.theta_neg1, -2.094))
-    assert(np.isclose(thetas.theta_h, 2.739))
-    assert(np.isclose(thetas.theta_1, 0.368))
+    assert(np.isclose(thetas.theta_0, -1.047, atol=1e-3))
+    assert(np.isclose(thetas.theta_2, 0.542, atol=1e-3))
+    assert(np.isclose(thetas.theta_gs, 0.505, atol=1e-3))
+    assert(np.isclose(thetas.theta_neg1, -2.094, atol=1e-3))
+    assert(np.isclose(thetas.theta_h, 2.739, atol=1e-3))
+    assert(np.isclose(thetas.theta_1, 0.368, atol=1e-3))
